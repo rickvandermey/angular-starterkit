146e49a3b3afc5c435d607817db11c7751f8a788
Rick van der Meij <rick.vandermeij@newstory.nl>
2021-09-09 10:06:54 +0200

    [update-deps] update to latest dependencies

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -207,6 +207,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="components/BaseComponent.html" data-type="entity-link">BaseComponent</a>
                             </li>
+                            <li class="link">
+                                <a href="components/ErrorPageComponent.html" data-type="entity-link">ErrorPageComponent</a>
+                            </li>
                         </ul>
                     </li>
                     <li class="chapter">
