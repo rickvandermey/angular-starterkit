96a177c2146743819b21fd7440eb6b103e51dbcf
Rick van der Meij <rick.vandermeij@newstory.nl>
2021-12-02 09:48:11 +0100

    [angular 13] update first batch of deps

--- apps/starterkit/src/polyfills.ts
+++ apps/starterkit/src/polyfills.ts
@@ -33,9 +33,6 @@
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/weak-map';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js'; // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
